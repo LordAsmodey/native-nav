f261492f004f4e7b0b2c99dd2a35667cb6391a2e
Oleg Vetrov <oleg94038@gmail.com>
2022-10-27 14:55:01 +0300

    Added navigation types

--- src/navigation/screens/MainScreen/index.tsx
+++ src/navigation/screens/MainScreen/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
+import { ProfileScreenNavigationProp } from '../../../interfaces/Navigation';
 
 export const MainScreen: React.FC = () => {
-  const {navigate} = useNavigation();
+  const {navigate} = useNavigation<ProfileScreenNavigationProp>();
   return (
     <View style={styles.container}>
       <Text>MainScreen</Text>
       <TouchableOpacity>
-        <Button title="Go next" onPress={() => navigate('ProductScreen')}/>
+        <Button title="Go next" onPress={() => navigate('ProductScreen', 'Title')}/>
       </TouchableOpacity>
     </View>
   );
